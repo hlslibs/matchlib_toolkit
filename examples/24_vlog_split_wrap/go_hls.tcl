f10d03c282ce135b872e9c8dfab01160965647a1
David Burnette <hlslibs@mentor.com>
2024-05-23 11:42:57 -0700

    corrected include paths

--- examples/24_vlog_split_wrap/go_hls.tcl
+++ examples/24_vlog_split_wrap/go_hls.tcl
@@ -4,9 +4,9 @@ options defaults
 
 options set /Input/CppStandard c++11
 options set /Input/CompilerFlags " "
-options set /Input/SearchPath $sfd -append
-options set /Input/SearchPath $sfd/../../include -append
+options set /Input/SearchPath {$MGC_HOME/shared/examples/matchlib/toolkit/include} -append
 options set /Input/SearchPath {$MGC_HOME/shared/pkgs/matchlib/cmod/include} -append
+options set /Input/SearchPath {$MGC_HOME/shared/pkgs/boostpp/pp/include} -append
 
 project new
 
